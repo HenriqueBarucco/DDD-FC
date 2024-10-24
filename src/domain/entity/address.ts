cf8433a81d87ce87afce69a6842def4207a6c399
Henrique Barucco <henrique_barucco@hotmail.com>
2024-10-24 13:55:17 -0300

    feat: implement customer repository

--- src/domain/entity/address.ts
+++ src/domain/entity/address.ts
@@ -1,8 +1,8 @@
 export default class Address {
-  _street: string
-  _number: number
-  _zip: string
-  _city: string
+  _street: string = ''
+  _number: number = 0
+  _zip: string = ''
+  _city: string = ''
 
   constructor(street: string, number: number, zip: string, city: string) {
     this._street = street
@@ -11,6 +11,22 @@ export default class Address {
     this._city = city
 
     this.validate()
+  }
+
+  get street(): string {
+    return this._street
+  }
+
+  get number(): number {
+    return this._number
+  }
+
+  get zip(): string {
+    return this._zip
+  }
+
+  get city(): string {
+    return this._city
   }
 
   validate() {
@@ -29,6 +45,6 @@ export default class Address {
   }
 
   toString() {
-    return `${this._street} ${this._number}, ${this._zip} ${this._city}`
+    return `${this._street}, ${this._number}, ${this._zip} ${this._city}`
   }
 }
