582fa74dfa31d9ef509cc3847838c4baa4c228ac
Henrique Barucco <henrique_barucco@hotmail.com>
2024-11-15 16:53:46 -0300

    test(domain): validate error notification for more than one error

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -15,6 +15,12 @@ describe('Product unit tests', () => {
     )
   })
 
+  it('should throw error when name is empty, id are empty and price is less than zero', () => {
+    expect(() => new Product('', '', -1)).toThrow(
+      'product: Id is required,product: Name is required,product: Price must be greater than zero',
+    )
+  })
+
   it('should change name', () => {
     const product = new Product('123', 'Product 1', 100)
     product.changeName('Product 2')
